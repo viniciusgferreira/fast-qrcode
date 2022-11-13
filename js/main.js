e71f89d52174d2cdba133ca7252c18e12ae63560
ViniciusGFerreira <vinicius.gf@outlook.com>
2022-11-13 16:41:23 -0300

    Button to Generate QR code, script to return the <img> with a result of API call [gopr.me]

--- js/main.js
+++ js/main.js
@@ -5,5 +5,5 @@
 
 function generateQRcode () {
     document.getElementById("qr-result-area").innerHTML = "<img src=\"https://api.qrserver.com/v1/create-qr-code/?data=google.com.br\">"
-    //document.getElementById("qr-result-area").innerHTML = "<p>result</p>";
+    
 }

1938bde8f5850deb7dc77c7eba94c6c1f029aae0
ViniciusGFerreira <vinicius.gf@outlook.com>
2022-11-14 13:39:31 -0300

    Fixed download button getting rendered before de qr code itself

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 function generateQRcode () {
     let url = document.getElementById("inputUrl").value;
-    let qrcode = "<img src=\"https://api.qrserver.com/v1/create-qr-code/?data=" + url + "\">";
+    let qrcode = "<img onload=\"generateDownload()\" src=\"https://api.qrserver.com/v1/create-qr-code/?data=" + url + "\">";
     document.getElementById("qr-code").innerHTML = qrcode;
-    generateDownload();    
+   
 }
 
 function generateDownload () {

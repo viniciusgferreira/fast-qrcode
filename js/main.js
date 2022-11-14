80e41d045cbd885606508558cb2e4c3c53998b84
ViniciusGFerreira <vinicius.gf@outlook.com>
2022-11-14 13:33:59 -0300

    Added Download Button after qr-code generating

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-/* function generateQRcode (form) {
-    fetch("http://api.qrserver.com/v1/create-qr-code/")
-    .then()
-} */
-
 function generateQRcode () {
     let url = document.getElementById("inputUrl").value;
     let qrcode = "<img src=\"https://api.qrserver.com/v1/create-qr-code/?data=" + url + "\">";
-    document.getElementById("qr-result-area").innerHTML = qrcode;
-    
+    document.getElementById("qr-code").innerHTML = qrcode;
+    generateDownload();    
+}
+
+function generateDownload () {
+    document.getElementById("download").innerHTML = "<button type=\"button\" onclick=\"downloadCode()\">Download</button>";
+}
+
+function downloadCode () {
+    //DOWNLOAD
 }
